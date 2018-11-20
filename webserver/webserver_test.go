43a47a5f2bafe19b4ddfdd8b84dea5e66b5d5115
Michael Skrzypietz <michael.s97@web.de>
2018-11-20 13:51:34 +0100

    Added user registration test

--- webserver/webserver_test.go
+++ webserver/webserver_test.go
@@ -1,6 +1,10 @@
 package webserver
 
 import (
+	"TicketSystem/config"
+	"bytes"
+	"net/http"
+	"net/http/httptest"
 	"testing"
 )
 
@@ -10,4 +14,26 @@ func TestIndexPage(t *testing.T) {
 
 func TestExecuteTemplate(t *testing.T) {
 	//response := ExecuteTemplate(http.ResponseWriter(), *http.Request{}, "../templates/login.html", nil)
+}
+
+func TestServeUserRegistration(t *testing.T) {
+	config.UsersPath = "../data/users/users.xml"
+
+	req, err := http.NewRequest("POST", "/register", bytes.NewBuffer([]byte(`{"newUsername": "Test123", "newPassword1"": 123, "newPassword2"": 123"}`)))
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	rr := httptest.NewRecorder()
+	handler := http.HandlerFunc(ServeUserRegistration)
+
+	handler.ServeHTTP(rr, req)
+
+	if status := rr.Code; status != http.StatusAccepted {
+		t.Errorf("Handler returned the wrong status code... got %v and expected %v", status, http.StatusAccepted)
+	}
+
+	// TODO: Check if user is registered / available in users.xml with xmlIO.CheckUser
+
+	// TODO: Remove User or delete users.xml file??
 }
