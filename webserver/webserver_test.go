4297d48c1afcf3f416a7e9ed7d07f1757af6ca49
Michael Skrzypietz <michael.skr97@gmail.com>
2019-01-05 19:51:30 +0100

    add some webserver tests

--- webserver/webserver_test.go
+++ webserver/webserver_test.go
@@ -222,3 +222,140 @@ func TestServeUserRegistrationSuccess(t *testing.T) {
 	assert.Nil(t, err)
 	assert.Equal(t, "/", resultURL.Path)
 }
+
+func TestServeSignInShowTemplate(t *testing.T) {
+	setup()
+	defer teardown()
+
+	form := url.Values{}
+
+	req := httptest.NewRequest(http.MethodPost, "/signIn", strings.NewReader(form.Encode()))
+	req.Header.Set("Content-Type", "application/x-www-form-urlencoded; param=value")
+	req.Form = form
+
+	rr := httptest.NewRecorder()
+	handler := http.HandlerFunc(ServeSignIn)
+	handler.ServeHTTP(rr, req)
+
+	assert.Equal(t, rr.Code, http.StatusOK)
+}
+
+func createUser(username, password string) {
+	form := url.Values{}
+	form.Add("username", username)
+	form.Add("password1", password)
+	form.Add("password2", password)
+
+	req := httptest.NewRequest(http.MethodPost, "/signUp", strings.NewReader(form.Encode()))
+	req.Header.Set("Content-Type", "application/x-www-form-urlencoded; param=value")
+	req.Form = form
+
+	rr := httptest.NewRecorder()
+	handler := http.HandlerFunc(ServeUserRegistration)
+	handler.ServeHTTP(rr, req)
+}
+
+func TestServeSignInInvalidCredentials(t *testing.T) {
+	setup()
+	defer teardown()
+
+	form := url.Values{}
+	form.Add("username", "Tes23")
+	form.Add("password", "A3456")
+
+	req := httptest.NewRequest(http.MethodPost, "/signIn", strings.NewReader(form.Encode()))
+	req.Header.Set("Content-Type", "application/x-www-form-urlencoded; param=value")
+	req.Form = form
+
+	rr := httptest.NewRecorder()
+	handler := http.HandlerFunc(ServeSignIn)
+	handler.ServeHTTP(rr, req)
+
+	assert.Equal(t, rr.Code, http.StatusFound)
+	resultURL, err := rr.Result().Location()
+	assert.Nil(t, err)
+	assert.Equal(t, utils.ErrorUserLogin.ErrorPageUrl(), resultURL.Path)
+}
+
+func TestServeSignInSuccessWithHomeRedirect(t *testing.T) {
+	setup()
+	defer teardown()
+
+	createUser("Test123", "Aa!123456")
+
+	form := url.Values{}
+	form.Add("username", "Test123")
+	form.Add("password", "Aa!123456")
+
+	req := httptest.NewRequest(http.MethodPost, "/signIn", strings.NewReader(form.Encode()))
+	req.Header.Set("Content-Type", "application/x-www-form-urlencoded; param=value")
+	req.Form = form
+
+	rr := httptest.NewRecorder()
+	handler := http.HandlerFunc(ServeSignIn)
+	handler.ServeHTTP(rr, req)
+
+	assert.Equal(t, rr.Code, http.StatusFound)
+	resultURL, err := rr.Result().Location()
+	assert.Nil(t, err)
+	assert.Equal(t, "/", resultURL.Path)
+}
+
+func TestServeSignInSuccessWithRequestedURLRedirect(t *testing.T) {
+	setup()
+	defer teardown()
+
+	createUser("Test123", "Aa!123456")
+	requestedURL := "/ticket/new"
+
+	form := url.Values{}
+	form.Add("username", "Test123")
+	form.Add("password", "Aa!123456")
+
+	req := httptest.NewRequest(http.MethodPost, "/signIn", strings.NewReader(form.Encode()))
+	req.Header.Set("Content-Type", "application/x-www-form-urlencoded; param=value")
+	req.Form = form
+	req.AddCookie(&http.Cookie{
+		Name:     "requested-url-while-not-authenticated",
+		Value:    requestedURL,
+		Path:     "/",
+		HttpOnly: true,
+		MaxAge:   60,
+	})
+
+	rr := httptest.NewRecorder()
+	handler := http.HandlerFunc(ServeSignIn)
+	handler.ServeHTTP(rr, req)
+
+	assert.Equal(t, rr.Code, http.StatusFound)
+	resultURL, err := rr.Result().Location()
+	assert.Nil(t, err)
+	assert.Equal(t, requestedURL, resultURL.Path)
+}
+
+/*
+func TestServeTicketCreationInvalidInput(t *testing.T) {
+	setup()
+	defer teardown()
+
+	createUser("Test123", "Aa!123456")
+	requestedURL := "/ticket/new"
+
+	form := url.Values{}
+	form.Add("email", "mustermann@gmail.com")
+	form.Add("subject", "PC Issue")
+	form.Add("message", "I have issues with my pc...")
+
+	req := httptest.NewRequest(http.MethodPost, "/signIn", strings.NewReader(form.Encode()))
+	req.Header.Set("Content-Type", "application/x-www-form-urlencoded; param=value")
+	req.Form = form
+
+	rr := httptest.NewRecorder()
+	handler := http.HandlerFunc(ServeTicketCreation)
+	handler.ServeHTTP(rr, req)
+
+	assert.Equal(t, rr.Code, http.StatusFound)
+	resultURL, err := rr.Result().Location()
+	assert.Nil(t, err)
+	assert.Equal(t, requestedURL, resultURL.Path)
+}*/
