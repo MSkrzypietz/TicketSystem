b18d3bf62848a368a3cab6fe01934dbc1e233492
Michael Skrzypietz <michael.skr97@gmail.com>
2019-01-06 16:58:12 +0100

    add console tool to test the rest api that returns the emails to be sent

--- cmd/msgPullTool/main.go
+++ cmd/msgPullTool/main.go
@@ -1,5 +1,33 @@
 package main
 
-func main() {
+import (
+	"TicketSystem/utils"
+	"TicketSystem/webserver"
+	"encoding/xml"
+	"fmt"
+	"log"
+	"net/http"
+	"net/http/httptest"
+)
 
+func main() {
+	req := httptest.NewRequest(http.MethodGet, "/mails", nil)
+	rr := httptest.NewRecorder()
+	handler := http.HandlerFunc(webserver.ServeMailsAPI)
+	handler.ServeHTTP(rr, req)
+
+	var mails utils.Response
+	err := xml.NewDecoder(rr.Result().Body).Decode(&mails)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Printf("There are %d E-Mails to be sent:\n\n", len(mails.Data))
+	for _, mail := range mails.Data {
+		fmt.Printf("ID: %d\n", mail.MailId)
+		fmt.Printf("E-Mail: %s\n", mail.Mail)
+		fmt.Printf("Subject: %s\n", mail.Caption)
+		fmt.Printf("Message: %s\n", mail.Message)
+		fmt.Println()
+	}
 }
