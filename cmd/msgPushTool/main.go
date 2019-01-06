e5241c4a3e1eb90d5bfd19416d96d9e67cb01d1f
Michael Skrzypietz <michael.skr97@gmail.com>
2019-01-06 18:30:57 +0100

    add console tool to send tickets to the ticket system

--- cmd/msgPushTool/main.go
+++ cmd/msgPushTool/main.go
@@ -1,7 +1,44 @@
 package main
 
-import "fmt"
+import (
+	"TicketSystem/XML_IO"
+	"TicketSystem/utils"
+	"TicketSystem/webserver"
+	"bytes"
+	"encoding/xml"
+	"flag"
+	"fmt"
+	"log"
+	"net/http"
+	"net/http/httptest"
+)
 
 func main() {
-	fmt.Printf("test")
+	mailAddress := flag.String("mail", "test@gmail.com", "Email Address")
+	subject := flag.String("subject", "Test Caption", "Ticket Subject")
+	msg := flag.String("message", "Test Message", "Ticket Message")
+	flag.Parse()
+
+	mail := XML_IO.Mail{Mail: *mailAddress, Caption: *subject, Message: *msg}
+	buf, err := xml.Marshal(mail)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	req := httptest.NewRequest(http.MethodPost, "/mails", bytes.NewBuffer(buf))
+	rr := httptest.NewRecorder()
+	handler := http.HandlerFunc(webserver.ServeMailsAPI)
+	handler.ServeHTTP(rr, req)
+
+	var response utils.Response
+	err = xml.NewDecoder(rr.Result().Body).Decode(&response)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	if response.Success {
+		fmt.Println("Successfully received the email.")
+	} else {
+		fmt.Println("We were not able to store your email. Please try it again!")
+	}
 }
