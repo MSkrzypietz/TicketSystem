0e503629befcc941b252e4acd11696e138b9353b
Michael Skrzypietz <michael.skr97@gmail.com>
2019-01-09 11:38:29 +0100

    add prevention for email ping pon

--- webserver/handlers.go
+++ webserver/handlers.go
@@ -7,6 +7,7 @@ import (
 	"net/http"
 	"path"
 	"strconv"
+	"time"
 )
 
 func ServeTickets(w http.ResponseWriter, r *http.Request) {
@@ -349,7 +350,9 @@ func getMails(w http.ResponseWriter, _ *http.Request) {
 
 	var mails []utils.MailData
 	for _, mail := range rawMails.Maillist {
-		//if time.Since(mail.LastReadAttemptDate).Minutes() &&
+		if preventMailPingPong(mail) {
+			continue
+		}
 
 		err := mail.IncrementReadAttemptsCounter()
 		if err != nil {
@@ -362,6 +365,16 @@ func getMails(w http.ResponseWriter, _ *http.Request) {
 	}
 
 	utils.RespondWithXML(w, http.StatusOK, utils.Response{Meta: utils.MetaData{Code: http.StatusOK, Message: "OK"}, Data: mails})
+}
+
+// Checks depending on the LastReadAttemptDate if the ReadAttemptCounter is tolerable to prevent email ping pong
+func preventMailPingPong(mail utils.Mail) bool {
+	minsSinceLastReadAttempt := time.Since(mail.LastReadAttemptDate).Minutes()
+	return (mail.ReadAttemptCounter == 3 && minsSinceLastReadAttempt < 1) ||
+		(mail.ReadAttemptCounter == 5 && minsSinceLastReadAttempt < 5) ||
+		(mail.ReadAttemptCounter == 10 && minsSinceLastReadAttempt < 60) ||
+		(mail.ReadAttemptCounter == 25 && minsSinceLastReadAttempt < 120) ||
+		(mail.ReadAttemptCounter == 50 && minsSinceLastReadAttempt < 240)
 }
 
 func postMails(w http.ResponseWriter, r *http.Request) {
