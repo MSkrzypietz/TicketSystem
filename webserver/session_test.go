ccf01a4172b657e991028d0c896d036ba5193c2d
Michael Skrzypietz <michael.skr97@gmail.com>
2019-01-10 16:09:18 +0100

    add session tests

--- webserver/session_test.go
+++ webserver/session_test.go
@@ -1,9 +1,24 @@
 package webserver
 
 import (
+	"github.com/stretchr/testify/assert"
+	"net/http/httptest"
 	"testing"
 )
 
-func TestGetUserFromCookie(t *testing.T) {
+func TestCreateSessionCookie(t *testing.T) {
+	rr := httptest.NewRecorder()
+	CreateSessionCookie(rr, "TestID")
 
+	assert.Equal(t, 1, len(rr.Result().Cookies()))
+	assert.Equal(t, "TestID", rr.Result().Cookies()[0].Value)
+}
+
+func TestDestroySession(t *testing.T) {
+	rr := httptest.NewRecorder()
+	DestroySession(rr)
+
+	assert.Equal(t, 1, len(rr.Result().Cookies()))
+	assert.Equal(t, "session-id", rr.Result().Cookies()[0].Name)
+	assert.Equal(t, "", rr.Result().Cookies()[0].Value)
 }
